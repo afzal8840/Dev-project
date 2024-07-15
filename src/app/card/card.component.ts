90707152e5396444194d6488165886b435b814b4
afzal8840 <ansariafzal4457@gmail.com>
2024-07-15 23:36:09 +0530

    afzal3

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -22,7 +22,7 @@ export class CardComponent  implements OnInit{
     },
     {
       title: 'Angular',
-      des: 'Angular (also referred to as "Angular 2+") is a TypeScript-based, free and open-source single-page web application framework led by the Angular',
+      des: 'Angular (also referred to as "Angular 2") is a TypeScript-based, free and open-source single-page web application framework led by the Angular',
       img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_sw18ZEcCRWtfKNjFurvxjhSZigzDI2WST6fK2nIaqU8lmviyPvgG5lbj9sr9tHVI71Y&usqp=CAU'
     }
   ]
